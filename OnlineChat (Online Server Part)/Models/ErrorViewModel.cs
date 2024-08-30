5c1c6b35ba8fbe97f7b5b5bb788b696f0335be92
Suxrob Shadmanov <suxrobvjl0@gmail.com>
2024-08-31 04:21:41 +0500

    Add project files.

---
+++ OnlineChat (Online Server Part)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineChat__Online_Server_Part_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
